41dc61bd9b868cfde12fc64404fe11ec56e08ebe
evandux <edux93@gmail.com>
2019-07-15 18:16:06 -0600

    First commit

---
+++ web_map.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken = '{ACCESS TOKEN}';
+var map = new mapboxgl.Map({
+container: 'map', // container id
+style: '{your style URL}', // stylesheet location
+center: [-115.99,49.6773783], // starting position [lng, lat]
+zoom: 13 // starting zoom
+});
